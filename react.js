030f0e8c784f02c92147c7d8425aeb7637fbfca1
mayank341 <mkvasudev768@gmail.com>
2025-05-02 13:01:06 +0530

    react class1

--- react.js
+++ react.js
@@ -1,1 +1,40 @@
 
+// react is js library. created by facebook.
+// 
+// library is used for creating ui .
+// used to create single page applic.
+// can build modern ,fast single page application or websites wit react.
+
+// react is library, collection of codes Jquery is a library for example.
+
+// framework has restriction.rules regulation and constraints.
+// framework is complete package of code with its own functionalities and libraries.
+// a framework has its own rules.
+// dont have too much flexibility.
+// angular is example of framework.
+
+// js6:combination of two things html into js.
+// react is virtual dom.
+
+//heirarachy of dom:windows,document ,
+
+// benefit of virtual dom:
+// u make change in the Code 
+// dom eill be ocmpletely updated anad written . 
+// react first creates exact copy of dom.
+// only updates specific part in dom,
+// react copies only new parts of virtual dom to athe actual dom,ratger than copleteely rewritting them,
+// so reat is so much popular,it makes react a standard dwebpage.
+
+// react core concept;JSX
+// in JSx ,we write html tags with JSON,
+//  Syntax: const element=<h1>hello</h1>;
+
+// babel.
+// html tags are rendered directly inside into JSON,this approach make js Faster,
+
+
+
+
+
+
